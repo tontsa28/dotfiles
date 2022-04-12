e1e39d9b99b51a2522569eb3f74a1c97f8e0707a
tontsa28 <xdman7917@gmail.com>
2022-04-12 17:15:24 +0300

    chore: Update README.md

--- scripts/square.py
+++
@@ -1,6 +0,0 @@
-def square(n):
-    return n*n
-
-input = int(input("Give any integer: "))
-
-print(square(input))
--- shell/fetch.sh
+++
@@ -1,2 +0,0 @@
-#! /bin/sh
-find . -mindepth 1 -maxdepth 1 -type d -print -exec git -C {} fetch \;
